7ad751d137b380147faccae74475c00e734e0c4c
Tanner Sims <tannersims@hesitantlyhuman.com>
2025-06-29 01:07:09 -0600

    Fix redis error object to ensure that class methods also fail with improper imports

--- src/treelog/backends/__init__.py
+++ src/treelog/backends/__init__.py
@@ -4,10 +4,18 @@ try:
     from treelog.backends.redis import RedisReader, RedisWriter
 except ImportError:
 
-    def _error_fn(*args, **kwargs):
-        raise ImportError(
-            "To use the redis tree logging backend, please install the redis extras. (e.g. `pip install treelog[redis]`)"
-        )
+    class _RedisError:
+        def __call__(self, *args, **kwargs):
+            raise ImportError(
+                "To use the redis tree logging backend, please install the redis extras. (e.g. `pip install treelog[redis]`)"
+            )
 
-    RedisReader = _error_fn
-    RedisWriter = _error_fn
+        def __getattribute__(self, name):
+            raise ImportError(
+                "To use the redis tree logging backend, please install the redis extras. (e.g. `pip install treelog[redis]`)"
+            )
+
+    REDIS_ERROR = _RedisError()
+
+    RedisReader = REDIS_ERROR
+    RedisWriter = REDIS_ERROR
